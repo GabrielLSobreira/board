66068788fd64c9b47bdb81031fe89e40b7f0928d
GabrielLSobreira <gabriellaranjasobreira@gmail.com>
2022-02-14 13:17:45 -0300

    feat: adicionando sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import styles from '../styles/styles.module.scss';
+
 export default function Home() {
   return (
     <div>
-      <h1>Board</h1>
+      <h1 className={styles.title}>Board</h1>
     </div>
   );
 }
