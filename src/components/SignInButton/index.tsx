3e9ec273e31e5b445036c96b779126c1f1472ab9
GabrielLSobreira <gabriellaranjasobreira@gmail.com>
2022-02-15 12:58:31 -0300

    feat(app): autenticando usuário com o nextauth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,21 +1,29 @@
+import { signIn, signOut, useSession } from 'next-auth/client';
 import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
 export function SignInButton() {
-  const session = true;
+  const [session] = useSession();
+
+  console.log(session);
 
   return session ? (
-    <button type="button" className={styles.signInButton} onClick={() => {}}>
-      <img
-        src="https://sujeitoprogramador.com/steve.png"
-        alt="Foto do usuario"
-      />
-      Olá Gabriel
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
+      <img src={session.user.image} alt="Foto do usuario" />
+      Olá {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton} onClick={() => {}}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#FFb800" />
       Entrar com github
     </button>
