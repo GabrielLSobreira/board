fbfb81e77231f158be3184a23005efcddd3344e5
GabrielLSobreira <gabriellaranjasobreira@gmail.com>
2022-02-16 20:38:09 -0300

    feat(app): adicionando e configurando o firebase

---
+++ src/services/firebaseConnection.ts
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firebase';
+const firebaseConfig = {
+  apiKey: 'AIzaSyBiSNHBdMrWM_CNJqme-yWwuTcPS43jDp8',
+  authDomain: 'boardapp-46c34.firebaseapp.com',
+  projectId: 'boardapp-46c34',
+  storageBucket: 'boardapp-46c34.appspot.com',
+  messagingSenderId: '989216009203',
+  appId: '1:989216009203:web:0926d85b1b6238b6584086',
+};
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
